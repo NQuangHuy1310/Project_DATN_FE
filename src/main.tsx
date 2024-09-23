98c8ff28962cc9a446a18e03a09911d6d92f3e76
NguyenQuangHuy <kaisa13102004@gmail.com>
2024-09-23 11:49:00 +0700

    feature/config-api

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
+import { Toaster } from '@/components/ui/sonner'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Toaster } from '@/components/ui/toaster.tsx'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
-        <Toaster />
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <Toaster position="top-right" />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     </StrictMode>
 )

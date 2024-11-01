fb727f691da1e08b2159f6222b52695dcdf04e8c
LinhNB2004 <linhlinh26052004@gmail.com>
2024-11-01 23:11:19 +0700

    refactor/my-post-page

--- src/views/user/Posts/MyPosts/MyPosts.tsx
+++ src/views/user/Posts/MyPosts/MyPosts.tsx
@@ -9,6 +9,7 @@ import Loading from '@/components/Common/Loading/Loading'
 import { toast } from 'sonner'
 import { useNavigate } from 'react-router-dom'
 import routes from '@/configs/routes'
+import { getImagesUrl } from '@/lib'
 
 const MyPosts = () => {
     const navigate = useNavigate()
@@ -30,14 +31,14 @@ const MyPosts = () => {
     }
 
     return (
-        <div className="flex flex-col gap-5 rounded-md bg-white p-7">
+        <div className="flex flex-col gap-5 rounded-md bg-white p-10 px-20">
             <Tabs defaultValue="published" className="flex min-h-[500px] flex-col gap-3">
                 <TabsList className="grid w-full grid-cols-2 md:w-[400px]">
                     <TabsTrigger value="published">Đã xuất bản</TabsTrigger>
                     <TabsTrigger value="draft">Bản nháp</TabsTrigger>
                 </TabsList>
 
-                <div className="w-full md:w-[70%]">
+                <div className="w-full">
                     <TabsContent value="published" className="flex flex-col gap-3">
                         {data?.map((post, index) => {
                             const updatedAtString = post.published_at
@@ -46,20 +47,20 @@ const MyPosts = () => {
 
                             return (
                                 <div
-                                    className="flex flex-wrap items-start justify-between rounded-xl border-2 p-5"
+                                    className="flex flex-wrap items-start justify-between rounded border p-3 hover:shadow-md"
                                     key={index}
                                 >
-                                    <div className="flex flex-col gap-2">
-                                        <h3
-                                            className="cursor-pointer text-xl font-semibold"
-                                            onClick={handleNavigate(post.slug)}
-                                        >
-                                            {post.title}
-                                        </h3>
-                                        <div className="flex items-center gap-2">
-                                            <span className="text-darkGrey">Đã đăng vào {timeAgo} </span>
-                                            <BsDot className="text-darkGrey" />
-                                            <span className="text-darkGrey">1 phút đọc</span>
+                                    <div className="flex gap-3">
+                                        <img className="h-20 w-20 rounded border"  src={getImagesUrl(post.thumbnail)} alt="" />
+                                        <div className="flex flex-col gap-2">
+                                            <h3 className="cursor-pointer text-lg" onClick={handleNavigate(post.slug)}>
+                                                {post.title}
+                                            </h3>
+                                            <div className="flex items-center gap-1">
+                                                <span className="text-darkGrey">Đã xuất bản</span>
+                                                <BsDot className="text-darkGrey" />
+                                                <span className="text-darkGrey">1 thg 11</span>
+                                            </div>
                                         </div>
                                     </div>
                                     <DropdownMenu>

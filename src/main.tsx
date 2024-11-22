597e69f44e10397e58ae4f36c48baa5a06be9415
tai19062004 <nguyentutai1964@gmail.com>
2024-11-22 17:30:21 +0700

    fix/quiz-learning-and-highlightcode

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'highlight.js/styles/an-old-hope.min.css'
 
 const queryClient = new QueryClient({
     defaultOptions: {

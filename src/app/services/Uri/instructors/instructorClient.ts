e95d9e279507e17a910a78e8efdea0e4414f23c8
Le Dinh Dung <dev.dungld@gmail.com>
2024-12-05 09:34:27 +0700

    feature/intructor-client-search

--- src/app/services/Uri/instructors/instructorClient.ts
+++ src/app/services/Uri/instructors/instructorClient.ts
@@ -4,5 +4,6 @@ export const instructorClientUri = {
     ALL_INSTRUCTOR: (page: number, perPage?: number) =>
         `${INSTRUCTOR_URL}?page=${page}${perPage ? `&perPage=${perPage}` : ''}`,
     DETAIL_INSTRUCTOR: (instructorId: number) => `${INSTRUCTOR_URL}list-courses/${instructorId}`,
-    GET_TEACHER_MONTH: `${INSTRUCTOR_URL}list-teacher-month`
+    GET_TEACHER_MONTH: `${INSTRUCTOR_URL}list-teacher-month`,
+    GET_TEACHER_BY_SEARCH: (search: string) => `${INSTRUCTOR_URL}?search=${search}`
 }
